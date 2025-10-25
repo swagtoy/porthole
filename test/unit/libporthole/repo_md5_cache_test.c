c295e7dbd034638b646710275f2da1c2318dd52f
Swagtoy <me@ow.swag.toys>
2025-10-25 03:38:40 -0400

    test: md5-cache test
    For later
    
    Signed-off-by: Swagtoy <me@ow.swag.toys>

---
+++ test/unit/libporthole/repo_md5_cache_test.c
@@ -0,0 +1,36 @@
+#include <assert.h>
+#include <stdio.h>
+#include <repo.h>
+#include "_debug_prints.h"
+#include "atom.h"
+#include <sys/types.h>
+#include <dirent.h>
+
+#define ROOT "/var/db/repos/gentoo/metadata/md5-cache"
+
+int
+main()
+{
+	char buf[512] = {0};
+	char path[2048] = { 0 };
+	DIR *root = opendir(ROOT);
+	DIR *cat, *pkg;
+
+	struct dirent *ent, *ent2;
+	while ((ent = readdir(root)))
+	{
+		snprintf(path, 2048-1, ROOT "/%s", ent->d_name);
+		cat = opendir(path);
+		while ((ent2 = readdir(cat)))
+		{
+			// parse ebuild filename now!
+			ph_atom_t atom;
+			char *atomstr;
+			ph_atom_parse_string(ent2->d_name, &atom, PH_ATOM_PARSE_STRIP_EBUILD);
+		}
+		closedir(cat);
+	}
+	closedir(root);
+	
+	return 0;
+}
